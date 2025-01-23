32f883624bd187568b9a59ccd49e90f9e2883cb1
Dima Kostenko <dkostenko44@gmail.com>
2025-01-23 15:29:51 +0300

    add pagination

--- src/components/Pagination.jsx
+++ src/components/Pagination.jsx
@@ -3,11 +3,20 @@ import { FaAngleRight, FaAngleLeft } from "react-icons/fa6";
 export default function Pagination({ page, setPage }) {
   const nextPage = (page)=>{
     setPage(page+1)
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
   }
+  
   const backPage = (page)=>{
     if(page>1){
       setPage(page-1)
     }
+    window.scrollTo({
+      top: 0,
+      behavior: "smooth"
+    })
   }
   return (
     <div className="navigate">
